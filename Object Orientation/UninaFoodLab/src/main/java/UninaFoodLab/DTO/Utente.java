4e3c3d79375f71b4e8a6e51272bd53b943daeb57
Giuseppe <espositogiuseppe2003@outlook.it>
2025-06-17 01:32:21 +0200

    --

--- Object Orientation/UninaFoodLab/src/main/java/UninaFoodLab/DTO/Utente.java
+++ Object Orientation/UninaFoodLab/src/main/java/UninaFoodLab/DTO/Utente.java
@@ -1,5 +1,90 @@
 package UninaFoodLab.DTO;
 
+import java.util.Date;
+
 public abstract class Utente
 {
+    private String username;
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+    private Date dataDiNascita;
+    private String luogoDiNascita;
+    private String email;
+    private String password;
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getCognome()
+    {
+        return cognome;
+    }
+
+    public void setCognome(String cognome)
+    {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale()
+    {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale)
+    {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public Date getDataDiNascita()
+    {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(Date dataDiNascita)
+    {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public String getLuogoDiNascita()
+    {
+        return luogoDiNascita;
+    }
+
+    public void setLuogoDiNascita(String luogoDiNascita)
+    {
+        this.luogoDiNascita = luogoDiNascita;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
