f693e2e4b68cb05bdf27e4e2354566c423e69b3e
virginiaesposito <espo.vir.a@gmail.com>
2025-06-18 03:01:45 +0200

    classi di virginia

--- Object Orientation/UninaFoodLab/src/main/java/UninaFoodLab/DTO/Adesione.java
+++ Object Orientation/UninaFoodLab/src/main/java/UninaFoodLab/DTO/Adesione.java
@@ -1,5 +1,56 @@
 package UninaFoodLab.DTO;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class Adesione
 {
+    private LocalDate dataAdesione;
+    Partecipante partecipante;
+    SessionePratica sessione;
+
+    public Adesione(LocalDate dataAdesione, Partecipante partecipante, SessionePratica sessione)
+    {
+        this.dataAdesione = dataAdesione;
+        this.partecipante = partecipante;
+        this.sessione = sessione;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null || getClass() != o.getClass()) return false;
+        Adesione ad = (Adesione) o;
+        return dataAdesione.equals(ad.dataAdesione) && partecipante.equals(ad.partecipante) && sessione.equals(ad.sessione);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(dataAdesione, partecipante, sessione);
+    }
+
+    public LocalDate getDataAdesione() {
+        return dataAdesione;
+    }
+
+    public void setDataAdesione(LocalDate dataAdesione) {
+        this.dataAdesione = dataAdesione;
+    }
+
+    public Partecipante getPartecipante() {
+        return partecipante;
+    }
+
+    public void setPartecipante(Partecipante partecipante) {
+        this.partecipante = partecipante;
+    }
+
+    public SessionePratica getSessione() {
+        return sessione;
+    }
+
+    public void setSessione(SessionePratica sessione) {
+        this.sessione = sessione;
+    }
 }
--- Object Orientation/UninaFoodLab/src/main/java/UninaFoodLab/DTO/Argomento.java
+++ Object Orientation/UninaFoodLab/src/main/java/UninaFoodLab/DTO/Argomento.java
@@ -2,4 +2,15 @@ package UninaFoodLab.DTO;
 
 public class Argomento
 {
+    private String nome;
+
+    public Argomento (String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getNome ()
+    {
+        return nome;
+    }
 }
